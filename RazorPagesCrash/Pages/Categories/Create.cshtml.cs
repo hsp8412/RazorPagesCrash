8496d5ff29bc6fb835a3d8114463dca0f910b6f2
Sipeng He <55742938+hsp8412@users.noreply.github.com>
2022-10-10 14:48:05 -0600

    Save progress

--- RazorPagesCrash/Pages/Categories/Create.cshtml.cs
+++ RazorPagesCrash/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesCrash.Data;
+using RazorPagesCrash.Model;
 
 namespace RazorPagesCrash.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set;}
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(String.Empty, "The DisplayOrder cannot exactly match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                await _db.Category.AddAsync(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
